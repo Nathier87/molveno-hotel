63c18bda2fb870a8c12cca5e1d54afb136589488
mmajafi <mahdinajafi2465@gmail.com>
2020-01-15 14:44:45 +0100

    this is my part1

--- src/com/capgemini/molveno/hotel/model/Reservation.java
+++ src/com/capgemini/molveno/hotel/model/Reservation.java
@@ -5,31 +5,14 @@ import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 
 public class Reservation {
-    public static void main(String[] args) {
-        LocalDate entryDate;
-        int room;
-        String name;
+
+        LocalDateTime entryDate;
+        int roomNumber;
+        String guestName;
         String emailAddress;
-        LocalDateTime dateTime;
-        int result= 0;
-        public int room (int roomNumber) {
-            switch (roomNumber) {
-                case 1:
-                    int single;
-                    room = single;
-                    break;
-                case 2:
-                    int twin;
-                    room = twin;
-                    break;
-                case 3:
-                    int familyRoom;
-                    room = familyRoom;
+        String homeAddress;
+        LocalDateTime checkOut;
+        LocalDateTime reservationDate;
 
-
-        }
-
-
-    }
 
 
---
+++ src/com/capgemini/molveno/hotel/model/info.java
@@ -0,0 +1,24 @@
+package com.capgemini.molveno.hotel.model;
+
+public class info {
+    public static void main(String[] args) {
+
+    }
+    int result= 0;
+    public int room (int roomNumber) {
+        switch (roomNumber) {
+            case 1:
+                int single;
+                room = single;
+                break;
+            case 2:
+                int twin;
+                room = twin;
+                break;
+            case 3:
+                int familyRoom;
+                room = familyRoom;
+
+
+        }
+
