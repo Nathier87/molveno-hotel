f6fe4b9327895c02313096b823282c575a78cb6f
mmajafi <mahdinajafi2465@gmail.com>
2020-01-15 14:29:38 +0100

    this is my part

--- src/com/capgemini/molveno/hotel/model/Reservation.java
+++ src/com/capgemini/molveno/hotel/model/Reservation.java
@@ -1,6 +1,7 @@
 package com.capgemini.molveno.hotel.model;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
 
 public class Reservation {
@@ -9,6 +10,26 @@ public class Reservation {
         int room;
         String name;
         String emailAddress;
+        LocalDateTime dateTime;
+        int result= 0;
+        public int room (int roomNumber) {
+            switch (roomNumber) {
+                case 1:
+                    int single;
+                    room = single;
+                    break;
+                case 2:
+                    int twin;
+                    room = twin;
+                    break;
+                case 3:
+                    int familyRoom;
+                    room = familyRoom;
+
+
+        }
+
+
     }
 
 
